1b70c0570e00d41fde8a711ef1732a7962dd54e1
AlmadhA <almadharafif.personal@gmail.com>
2025-05-21 11:08:49 +0700

    Update

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
-# config.py
+import os
+import json
 
-SPREADSHEET_ID = "ISI_DENGAN_SPREADSHEET_ID_MU"
-GOOGLE_CREDENTIALS_PATH = "credentials/service_account.json"
+SPREADSHEET_ID = os.getenv("1PAgc4t9RBbPLpnsoWbC1rgcwH6bX6jK8IcGel-b4cG4")
+
+# Simpan kredensial dari secrets ke file sementara
+creds = json.loads(os.getenv("GOOGLE_CREDENTIALS"))
+with open("temp_credentials.json", "w") as f:
+    json.dump(creds, f)
+
+GOOGLE_CREDENTIALS_PATH = "temp_credentials.json"
