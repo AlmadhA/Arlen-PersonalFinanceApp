6f8cf6148252fef57131b314e8d0e23629138e9b
AlmadhA <almadharafif.personal@gmail.com>
2025-05-21 15:51:02 +0700

    Update 1.1

--- sheets_service.py
+++ sheets_service.py
@@ -5,12 +5,15 @@ import pandas as pd
 
 def get_gspread_client():
     creds_info = st.secrets["gcp_service_account"]
-    creds = service_account.Credentials.from_service_account_info(creds_info)
+    creds = service_account.Credentials.from_service_account_info(
+        creds_info,
+        scopes=["https://www.googleapis.com/auth/spreadsheets"]  # penting!
+    )
     return gspread.authorize(creds)
 
 def read_records(sheet_name):
     client = get_gspread_client()
-    spreadsheet_id = st.secrets["gcp_service_account"]["spreadsheet_id"]
+    spreadsheet_id = st.secrets["spreadsheet_id"]  # Ambil dari root secrets
     sheet = client.open_by_key(spreadsheet_id).worksheet(sheet_name)
     records = sheet.get_all_records()
     return pd.DataFrame(records)
